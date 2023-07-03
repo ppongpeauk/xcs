a0e51e3622dc405ad998cfc9286f135b5d2940f3
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-07-03 00:35:17 -0400

    feat(*): initial commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  titleTemplate: "EVE XCS | {title}",
   reactStrictMode: true,
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import Footer from "@/components/Footer";
 import Home from "@/components/Home";
 import Nav from "@/components/Nav";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
     <>
+      <Head>
+        <title>EVE XCS | Home</title>
+        <meta name="description" content="EVE XCS" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Open Graph Protocol */}
+        <meta property="og:title" content="EVE XCS" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://xcs.restrafes.co/" />
+        <meta property="og:image" content="/images/hero3.jpg" />
+        <meta property="og:description" content="Control your access points with ease." />
+      </Head>
       <Nav />
       <Home />
       <Footer />
