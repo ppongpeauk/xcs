6b4b115c3c895ef6213fae37337b2e28a8b088b8
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-07-03 00:20:10 -0400

    feat(*): initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,26 @@
 import type { AppProps } from "next/app";
 
 // Chakra UI
-import { ChakraProvider, ColorModeScript, useColorModeValue } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeScript,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 // Theme
 import PageProgress from "@/components/PageProgress";
-import theme from "./theme";
-
-import { Golos_Text } from "next/font/google";
-const font = Golos_Text({ subsets: ["latin"] });
+import theme from "@/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <main className={font.className}>
-      <ChakraProvider>
-        <PageProgress />
-        <Component {...pageProps} />
-      </ChakraProvider>
-      </main>
+      <>
+        <ChakraProvider>
+          <PageProgress />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </>
     </>
   );
 }
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,6 +1,9 @@
+import { Golos_Text } from "next/font/google";
+const font = Golos_Text({ subsets: ["latin"] });
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
+    <main className={font.className}>
       {children}
     </main>
   );
