7c635b6eec17e9eb6dfb6ad6b37c76697d87bb9b
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-07-06 04:59:53 -0400

    feat(*) invitation stuff

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig, "[DEFAULT]");
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export default app;
