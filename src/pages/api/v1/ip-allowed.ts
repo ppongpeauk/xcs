76aee460fe15aad535bdfa1f90bae2fa824f949c
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-07-20 01:40:21 -0400

    feat(api) stuff

--- src/pages/api/v1/ip-allowed.ts
+++ src/pages/api/v1/ip-allowed.ts
@@ -11,6 +11,7 @@ const handler = async (
   if (geoInfoJson.status === "fail") {
     return res.status(500).json({
       success: false,
+      remote: req.socket.remoteAddress,
       message: "Failed to get IP address",
     });
   }
@@ -18,6 +19,7 @@ const handler = async (
   return res.status(200).json({
     success: true,
     ip: requestIP.getClientIp(req),
+    remote: req.socket.remoteAddress,
     isp: geoInfoJson.isp,
     info: geoInfoJson,
   });
