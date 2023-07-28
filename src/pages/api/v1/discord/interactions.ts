bb7d761caadd46f69cebedfd1bff5d3ae65345f6
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-07-28 19:34:07 -0400

    feat(api) discord interactions test

---
+++ src/pages/api/v1/discord/interactions.ts
@@ -0,0 +1,33 @@
+import clientPromise from "@/lib/mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const client = await clientPromise;
+    const db = client.db();
+
+    const { interaction } = req.body;
+
+    if (interaction.type === 1) {
+      res.status(200).json({
+        type: 1,
+      });
+    }
+
+    if (interaction.type === 2) {
+      const { name, options } = interaction.data;
+
+      if (name === "ping") {
+        res.status(200).json({
+          type: 4,
+          data: {
+            content: "Pong!",
+          },
+        });
+      }
+    }
+  }
+}
