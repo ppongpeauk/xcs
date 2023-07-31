5c4b219a6d110dc3fc8b8001c23cd3408f54dcca
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-07-31 12:38:20 -0400

    feat(admin) customer service universe reset

--- src/components/SettingsAdmin.tsx
+++ src/components/SettingsAdmin.tsx
@@ -26,6 +26,7 @@ import {
 } from '@chakra-ui/react';
 
 import { AiOutlineUser } from 'react-icons/ai';
+import { BiReset } from 'react-icons/bi';
 import { IoSave } from 'react-icons/io5';
 
 import { Field, Form, Formik } from 'formik';
@@ -34,6 +35,7 @@ import { useRouter } from 'next/router';
 import { useAuthContext } from '@/contexts/AuthContext';
 
 import InvitePlatformModal from '@/components/InvitePlatformModal';
+import LocationResetUniverseIdModal from '@/components/LocationResetUniverseIdModal';
 
 export default function SettingsProfile() {
   const { currentUser, refreshCurrentUser, user, isAuthLoaded } = useAuthContext();
@@ -54,10 +56,18 @@ export default function SettingsProfile() {
     }
   }, [currentUser, push, toast]);
 
+  // Platform Invite Creation Modal
   const {
     isOpen: platformInviteModalOpen,
     onOpen: platformInviteModalOnOpen,
     onClose: platformInviteModalOnClose
+  } = useDisclosure();
+
+  // Location Universe ID Reset Modal
+  const {
+    isOpen: locationUniverseIdResetModalOpen,
+    onOpen: locationUniverseIdResetModalOnOpen,
+    onClose: locationUniverseIdResetModalOnClose
   } = useDisclosure();
 
   return (
@@ -68,6 +78,10 @@ export default function SettingsProfile() {
         onClose={platformInviteModalOnClose}
         onCreate={() => {}}
       />
+      <LocationResetUniverseIdModal
+        isOpen={locationUniverseIdResetModalOpen}
+        onClose={locationUniverseIdResetModalOnClose}
+      />
       {isAuthLoaded && currentUser && (
         <Box w={'fit-content'}>
           <Heading
@@ -76,7 +90,7 @@ export default function SettingsProfile() {
           >
             Platform
           </Heading>
-          <Flex py={4}>
+          <Stack py={4}>
             <Button
               mb={2}
               leftIcon={<AiOutlineUser />}
@@ -84,130 +98,16 @@ export default function SettingsProfile() {
             >
               Create Registration Invite
             </Button>
-          </Flex>
-
-          {/* <Formik
-            initialValues={{
-              displayName: currentUser?.displayName,
-              username: currentUser?.username,
-              bio: currentUser?.bio,
-            }}
-            onSubmit={(values, actions) => {
-              user.getIdToken().then((token: string) => {
-                fetch("/api/v1/me", {
-                  method: "PATCH",
-                  headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                  },
-                  body: JSON.stringify({
-                    displayName: values.displayName,
-                    bio: values.bio,
-                  }),
-                })
-                  .then((res) => {
-                    if (res.status === 200) {
-                      return res.json();
-                    } else {
-                      return res.json().then((json: any) => {
-                        throw new Error(json.message);
-                      });
-                    }
-                  })
-                  .then((data) => {
-                    toast({
-                      title: data.message,
-                      status: "success",
-                      duration: 3000,
-                      isClosable: true,
-                    });
-                    refreshCurrentUser();
-                  })
-                  .catch((err) => {
-                    toast({
-                      title: "There was a problem while updating your profile.",
-                      description: err.message,
-                      status: "error",
-                      duration: 3000,
-                      isClosable: true,
-                    });
-                  })
-                  .finally(() => {
-                    actions.setSubmitting(false);
-                  });
-              });
-            }}
-          >
-            {(props) => (
-              <Form>
-                <HStack>
-                  <Field name="displayName">
-                    {({ field, form }: any) => (
-                      <FormControl w={"fit-content"}>
-                        <FormLabel>Display Name</FormLabel>
-                        <InputGroup mb={2}>
-                          <Input
-                            {...field}
-                            type="text"
-                            autoComplete="off"
-                            placeholder="Display Name"
-                            variant={"outline"}
-                          />
-                        </InputGroup>
-                      </FormControl>
-                    )}
-                  </Field>
-                  <Field name="username">
-                    {({ field, form }: any) => (
-                      <FormControl w={"fit-content"}>
-                        <FormLabel>Username</FormLabel>
-                        <InputGroup mb={2}>
-                          <Input
-                            {...field}
-                            isDisabled={true}
-                            type="text"
-                            autoComplete="off"
-                            placeholder="Username"
-                            variant={"outline"}
-                          />
-                        </InputGroup>
-                      </FormControl>
-                    )}
-                  </Field>
-                </HStack>
-                <Field name="bio">
-                  {({ field, form }: any) => (
-                    <FormControl>
-                      <FormLabel>Bio</FormLabel>
-                      <InputGroup mb={2}>
-                        <Textarea
-                          {...field}
-                          type="text"
-                          autoComplete="off"
-                          placeholder="Bio"
-                          variant={"outline"}
-                        />
-                      </InputGroup>
-                    </FormControl>
-                  )}
-                </Field>
-                <Stack
-                  direction={{ base: "column", md: "row" }}
-                  spacing={{ base: 2, md: 4 }}
-                  pt={2}
-                >
-                  <Button
-                    mb={2}
-                    isLoading={props.isSubmitting}
-                    leftIcon={<IoSave />}
-                    type={"submit"}
-                  >
-                    Save Changes
-                  </Button>
-                </Stack>
-              </Form>
-            )}
-          </Formik> */}
+            <FormControl>
+            <Button
+              mb={2}
+              leftIcon={<BiReset />}
+              onClick={locationUniverseIdResetModalOnOpen}
+            >
+              Reset Location Universe
+            </Button>
+            </FormControl>
+          </Stack>
         </Box>
       )}
     </>
