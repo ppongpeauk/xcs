c218bf504df97019012802f80b6f02ff0aa791ae
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-08-06 21:32:17 -0400

    chore(organizations) temp. redirect public pages to edit pages

--- src/pages/platform/organizations/[id]/index.tsx
+++ src/pages/platform/organizations/[id]/index.tsx
@@ -11,14 +11,20 @@ import {
   Text
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function OrganizationPublic() {
-  const { query } = useRouter();
+  const { query, push } = useRouter();
+
+  // TODO: public page
+  useEffect(() => {
+    push(`/platform/organizations/${query.id}/edit`);
+  }, []);
 
   return (
     <>
       <Container maxW="container.lg" py={8}>
-        <Heading as="h1" size="xl">Public page</Heading>
+        <Heading as="h1" size="xl">Public Page</Heading>
         <Box p={4}>
           <Button as={Link} href={`/platform/organizations/${query.id}/edit`} colorScheme="blue">Edit Organization</Button>
         </Box>
