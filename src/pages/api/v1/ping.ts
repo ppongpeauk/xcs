c7b653bdbed4a50c21ef8c97712f8ee48c91eac6
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-08-09 15:14:09 -0400

    feat(api) endpoint for checking uptime

---
+++ src/pages/api/v1/ping.ts
@@ -0,0 +1,11 @@
+import clientPromise from "@/lib/mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const mongoClient = await clientPromise;
+  const db = mongoClient.db(process.env.MONGODB_DB as string);
+  const statistics = db.collection('statistics');
+  const globalStatistics = await statistics.findOne({ id: 'global' });
+
+  return res.status(200).json({ success: true });
+}
